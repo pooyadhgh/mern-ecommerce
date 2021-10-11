92e59affcb15228faf54e9841ad53cdeb75ddb02
pooyadhgh <pooya.dhgh@gmail.com>
2021-10-11 18:56:12 +0330

    Add payment endpoint

--- server/controllers/orders.js
+++ server/controllers/orders.js
@@ -51,3 +51,33 @@ export const getOrderById = async (req, res, next) => {
 
   res.json(order);
 };
+
+export const updateOrderToPaid = async (req, res, next) => {
+  const orderId = req.params.id;
+  let order;
+
+  try {
+    order = await Order.findById(orderId);
+  } catch (err) {
+    const error = new HttpError('Could not find order', 500);
+    return next(error);
+  }
+
+  if (!order) {
+    const error = new HttpError('No order found', 404);
+    return next(error);
+  }
+
+  order.isPaid = true;
+  order.paidAt = Date.now();
+
+  let updatedOrder;
+  try {
+    updatedOrder = await Order.save();
+  } catch (err) {
+    const error = new HttpError('Could not save order', 500);
+    return next(error);
+  }
+
+  res.json(updatedOrder);
+};
--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,5 +1,9 @@
 import express from 'express';
-import { addOrder, getOrderById } from '../controllers/orders.js';
+import {
+  addOrder,
+  getOrderById,
+  updateOrderToPaid,
+} from '../controllers/orders.js';
 import { checkAuth } from '../middleware/auth.js';
 
 const router = express.Router();
@@ -7,5 +11,6 @@ const router = express.Router();
 // /api/orders/
 router.route('/').post(checkAuth, addOrder);
 router.route('/:id').get(checkAuth, getOrderById);
+router.route('/:id/pay').put(checkAuth, updateOrderToPaid);
 
 export default router;
